2260be86238b56c2dcfdff2e9de5257f5ccadd89
Saul Widrich <widrichs@mcmaster.ca>
2024-05-03 18:35:17 -0400

    R Markdown file and seperate r script for installing packages

---
+++ Packages.R
@@ -0,0 +1,12 @@
+install.packages("iidda.api"
+                 , repos = c(
+                   "https://canmod.r-universe.dev"
+                   , "https://cran.r-project.org"
+                 )
+)
+
+install.packages(c("dplyr", "lubridate", 
+                   "broom", "Hmisc", "readr", "jsonlite", "ggplot2", "patchwork", 
+                   "grDevices", "lomb"))
+
+
